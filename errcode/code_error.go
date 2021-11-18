416683036748583ea4b10e388695007e1bcfd8f5
mgsweet <mgsweet@126.com>
2021-11-18 20:03:13 +0800

    message -> msg

--- errcode/code_error.go
+++ errcode/code_error.go
@@ -4,27 +4,27 @@ import "fmt"
 
 // CodeError error with error code, and customize ErrorMessage
 type CodeError struct {
-	Code    int32
-	Message string
+	Code int32
+	Msg  string
 }
 
 // NewCodeError create a new CodeError
 func NewCodeError(code int32) *CodeError {
 	return &CodeError{
-		Code:    code,
-		Message: CodeDefaultDesc(code),
+		Code: code,
+		Msg:  CodeDefaultDesc(code),
 	}
 }
 
 // NewCodeErrorWithMessage create a new CodeError with message
-func NewCodeErrorWithMessage(code int32, message string) *CodeError {
+func NewCodeErrorWithMessage(code int32, msg string) *CodeError {
 	return &CodeError{
-		Code:    code,
-		Message: message,
+		Code: code,
+		Msg:  msg,
 	}
 }
 
 // Error implement error interface
 func (err *CodeError) Error() string {
-	return fmt.Sprintf("error code: %v, message: %v", err.Code, err.Message)
+	return fmt.Sprintf("error code: %v, message: %v", err.Code, err.Msg)
 }
