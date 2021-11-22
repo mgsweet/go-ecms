605424a0f1f5ac483a07c3f33ceeef312c53a5f5
mgsweet <mgsweet@126.com>
2021-11-22 16:52:00 +0800

    code gen 1.0

--- errcode/constant.go
+++ errcode/constant.go
@@ -1,13 +1,6 @@
 package errcode
 
-// CodeDefaultDesc returns the default description for the given error code.
-func CodeDefaultDesc(code int32) string {
-	if v, ok := codeDefaultDesc[code]; ok {
-		return v
-	} else {
-		return ""
-	}
-}
+// This file is generated. Do not edit.
 
 // Error code should start from 10 000 000 to ensure equal length.
 // Format: AA BBB CCC
@@ -17,16 +10,38 @@ func CodeDefaultDesc(code int32) string {
 const (
 	SUCCESS = 0
 
-	ReqParamIllegal = 2
-	Unauthorized    = 3
-	Forbidden       = 4
+	// Platform: "common 通用"
+	// Module: "service system 服务系统", Code format: 10 000 xxx
+	CommonServiceInternalError         = 10000000 // 通用服务内部错误 internal error
+	CommonServiceUnavailable           = 10000001 // 服务不可用 service unavailable
+	CommonServiceTimeout               = 10000002 // 服务自身任务超时 service timeout
+	CommonServiceBusy                  = 10000003 // 服务器繁忙 service is busy
+	CommonServiceDegradation           = 10000004 // 服务降级停用 service degradation
+	CommonServiceObsolete              = 10000005 // 服务已过期 service is obsolete
+	CommonServiceDependencyUnavailable = 10000006 // 服务依赖不可达 service dependency unavailable
+
 )
 
 // error code default description
 var codeDefaultDesc = map[int32]string{
 	SUCCESS: "success",
 
-	ReqParamIllegal: "Illegal request parameter",
-	Unauthorized:    "Unauthorized",
-	Forbidden:       "Forbidden",
+	// Platform: "common 通用"
+	// Module: "service system 服务系统"
+	CommonServiceInternalError:         "通用服务内部错误 internal error",
+	CommonServiceUnavailable:           "服务不可用 service unavailable",
+	CommonServiceTimeout:               "服务自身任务超时 service timeout",
+	CommonServiceBusy:                  "服务器繁忙 service is busy",
+	CommonServiceDegradation:           "服务降级停用 service degradation",
+	CommonServiceObsolete:              "服务已过期 service is obsolete",
+	CommonServiceDependencyUnavailable: "服务依赖不可达 service dependency unavailable",
+}
+
+// CodeDefaultDesc returns the default description for the given error code.
+func CodeDefaultDesc(code int32) string {
+	if desc, ok := codeDefaultDesc[code]; ok {
+		return desc
+	} else {
+		return ""
+	}
 }
