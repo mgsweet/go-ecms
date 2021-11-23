8e32f282098857d43345c848ad41644496e8b2f8
mgsweet <mgsweet@126.com>
2021-11-23 10:32:15 +0800

    fix build printing issue and remove Makefile

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	./build_site.sh
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 echo "Building go code..."
 go run go_code_gen.go
 go fmt errcode/constant.go
-echo "Building site code"
+echo "Building site code..."
 cd docs
 hugo -D
 cd ..
